f79b1fc41a3ecaf43f7b7c9ab7c880918a3a7182
Dale Morris <dalemorr@iu.edu>
2024-03-09 23:45:31 -0500

    Implemented file reading

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,20 @@
 #include "priority_queue.h"
+#include "airport_record.h"
 
 #include <fstream>
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::ifstream input("./data/air16.txt");
+    std::string line;
+    if (input.is_open()) {
+        while (getline(input, line)) {
+            std::cout << line << std::endl;
+        }
+    } else {
+        std::cerr << "Unable to open file" << std::endl;
+    }
+    input.close();
 
     return 0;
 }
