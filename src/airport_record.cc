cb052cdc2832794af84e1b51416e02d99fac3948
Dale Morris <dalemorr@iu.edu>
2024-03-12 22:05:51 -0400

    Implemented operator==

--- src/airport_record.cc
+++ src/airport_record.cc
@@ -21,12 +21,36 @@ AirportRecord::AirportRecord(string cd, string nm, string ct, string ctry, int l
     altitude = alt;
 }
 
+inline bool operator==(const AirportRecord &lhs, const AirportRecord &rhs) {
+    return lhs.code == rhs.code
+        && lhs.name == rhs.name
+        && lhs.city == rhs.city
+        && lhs.country == rhs.country
+        && lhs.latitudeDegrees == rhs.latitudeDegrees
+        && lhs.latitudeMinutes == rhs.latitudeMinutes
+        && lhs.latitudeSeconds == rhs.latitudeSeconds
+        && lhs.latitudeDirection == rhs.latitudeDirection
+        && lhs.longitudeDegrees == rhs.longitudeDegrees
+        && lhs.longitudeMinutes == rhs.longitudeMinutes
+        && lhs.longitudeSeconds == rhs.longitudeSeconds
+        && lhs.longitudeDirection == rhs.longitudeDirection
+        && lhs.altitude == rhs.altitude;
+}
+
+inline bool operator>(const AirportRecord &lhs, const AirportRecord &rhs) {
+    return lhs.altitude > rhs.altitude;
+}
+
+inline bool operator<(const AirportRecord &lhs, const AirportRecord &rhs) {
+    return lhs.altitude < rhs.altitude;
+}
+
 ostream& operator<<(ostream& os, const AirportRecord rec) {
     os << rec.code << " " 
         << rec.name << " " 
-        << rec.city << " "
-        << rec.country << " "
-        << rec.latitudeDegrees << " "
+        << rec.city << " " 
+        << rec.country << " " 
+        << rec.latitudeDegrees << " " 
         << rec.latitudeMinutes << " " 
         << rec.latitudeSeconds << " " 
         << rec.latitudeDirection << " " 
--- src/airport_record.h
+++ src/airport_record.h
@@ -1,30 +1,37 @@
-#ifndef AIRPORT_RECORD_H
-#define AIRPORT_RECORD_H
+#pragma once
 
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-typedef struct AirportRecord {
-    string code;
-    string name;
-    string city;
-    string country;
-    int latitudeDegrees;
-    int latitudeMinutes;
-    int latitudeSeconds;
-    string latitudeDirection;
-    int longitudeDegrees;
-    int longitudeMinutes;
-    int longitudeSeconds;
-    string longitudeDirection;
-    int altitude;
+typedef tuple<string, string, string, string, int, int, int, string, 
+    int, int, int, string, int> ARTuple;
 
-    AirportRecord(string cd, string nm, string ct, string ctry, int ltd, int ltm, int lts, 
+class AirportRecord {
+    public:
+        string code;
+        string name;
+        string city;
+        string country;
+        int latitudeDegrees;
+        int latitudeMinutes;
+        int latitudeSeconds;
+        string latitudeDirection;
+        int longitudeDegrees;
+        int longitudeMinutes;
+        int longitudeSeconds;
+        string longitudeDirection;
+        int altitude;
+
+        AirportRecord(string cd, string nm, string ct, string ctry, int ltd, int ltm, int lts, 
             string ltdir, int lnd, int lnm, int lns, string lndir, int alt);
 
-    friend ostream& operator<<(ostream& os, const AirportRecord rec);
-} AirportRecord;
+        friend ostream& operator<<(ostream& os, const AirportRecord rec);
 
-#endif // AIRPORT_RECORD_H
+        friend inline bool operator==(const AirportRecord &lhs, const AirportRecord &rhs);
+
+        friend inline bool operator>(const AirportRecord &lhs, const AirportRecord &rhs);
+
+        friend inline bool operator<(const AirportRecord &lhs, const AirportRecord &rhs);
+};
