8cb9fda25fa8643f05e957622d8be1915e45e2ec
Dale Morris <dalemorr@iu.edu>
2024-03-13 06:42:54 -0400

    Still don't use

--- src/airport_record/airport_record.h
+++ src/airport_record/airport_record.h
@@ -3,39 +3,37 @@
 #include <iostream>
 #include <string>
 
-class AirportRecord {
-    public:
-        std::string code;
-        std::string name;
-        std::string city;
-        std::string country;
-        int latitudeDegrees;
-        int latitudeMinutes;
-        int latitudeSeconds;
-        std::string latitudeDirection;
-        int longitudeDegrees;
-        int longitudeMinutes;
-        int longitudeSeconds;
-        std::string longitudeDirection;
-        int altitude;
+typedef struct AirportRecord {
+public:
+    std::string code;
+    std::string name;
+    std::string city;
+    std::string country;
+    int latitude_degrees;
+    int latitude_minutes;
+    int latitude_seconds;
+    std::string latitude_direction;
+    int longitude_degrees;
+    int longitude_minutes;
+    int longitude_seconds;
+    std::string longitude_direction;
+    int altitude;
 
-        AirportRecord(std::string cd, std::string nm, std::string ct, std::string ctry, int ltd,
-            int ltm, int lts, std::string ltdir, int lnd, int lnm, int lns, std::string lndir,
-            int alt);
+    AirportRecord(std::string cd, std::string nm, std::string ct, std::string ctry, int ltd,
+        int ltm, int lts, std::string ltdir, int lnd, int lnm, int lns, std::string lndir,
+        int alt);
 
-        friend std::ostream &operator<<(std::ostream &os, const AirportRecord rec);
+    friend std::ostream &operator<<(std::ostream &os, const AirportRecord& rec);
 
-        bool equals(const AirportRecord &rhs); // Compares all fields
+    bool operator==(const AirportRecord &rhs) const;
 
-        bool operator==(const AirportRecord &rhs); // Compares only altitudes
+    bool operator!=(const AirportRecord &rhs) const;
 
-        bool operator!=(const AirportRecord &rhs); // Compares only altitudes
+    bool operator<(const AirportRecord &rhs) const;
 
-        bool operator<(const AirportRecord &rhs);
+    bool operator>(const AirportRecord &rhs) const;
 
-        bool operator>(const AirportRecord &rhs);
+    bool operator<=(const AirportRecord &rhs) const;
 
-        bool operator<=(const AirportRecord &rhs);
-
-        bool operator>=(const AirportRecord &rhs);
-};
+    bool operator>=(const AirportRecord &rhs) const;
+} AirportRecord;
--- src/priority_queue/priority_queue.h
+++ src/priority_queue/priority_queue.h
@@ -6,19 +6,20 @@
 #include <vector>
 
 class PriorityQueue {
-    private:
-        std::vector<AirportRecord> keys;
+private:
+    std::vector<AirportRecord> keys;
 
-        void heapify(int i);
-        void swap(int i, int j);
-        int left(int i);
-        int right(int i);
-        int parent(int i);
-    public:
-        PriorityQueue(std::vector<AirportRecord> vec);
-        void insert(AirportRecord rec);
-        std::tuple<AirportRecord, int> maximum();
-        std::tuple<AirportRecord, int> extractMax();
-        void increaseKey(AirportRecord rec, int k);
-        int get_size();
+    void heapify(int i);
+    void swap(int i, int j);
+    static int left(int i);
+    static int right(int i);
+    static int parent(int i);
+public:
+    PriorityQueue();
+    explicit PriorityQueue(std::vector<AirportRecord> vec);
+    void insert(const AirportRecord& rec);
+    std::tuple<AirportRecord, int> maximum();
+    std::tuple<AirportRecord, int> extract_max();
+    void increase_key(const AirportRecord& rec, int k);
+    int get_size();
 };
