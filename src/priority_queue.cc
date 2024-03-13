1bb6f3cddb92e5554d4c089842a476f7c554a07c
Dale Morris <dalemorr@iu.edu>
2024-03-12 22:05:36 -0400

    Implemented constructor from a vector value

--- src/priority_queue.cc
+++ src/priority_queue.cc
@@ -1,42 +1,42 @@
 #include "priority_queue.h"
 #include "airport_record.h"
 
-void PriorityQueue::insert(AirportRecord *rec) {
+#include <algorithm>
 
+using namespace std;
+
+PriorityQueue::PriorityQueue(vector<AirportRecord> vec) {
+    this->keys = vec;
+    heapify();
 }
 
-std::tuple<AirportRecord*, int> PriorityQueue::maximum() {
-    AirportRecord rec = {
-        "",
-        "",
-        "",
-        "",
-        0, 0, 0,
-        "",
-        0, 0, 0,
-        "",
-        0,
-    };
+void PriorityQueue::increaseKey(AirportRecord rec, int k) {
+    auto location = find(this->keys.begin(), this->keys.end(), rec);
 
-    return std::make_tuple(&rec, 0);
+    if (k > location->altitude) {
+        location->altitude = k;
+    }
 }
 
-std::tuple<AirportRecord*, int> PriorityQueue::extractMax() {
-    AirportRecord rec = {
-        "",
-        "",
-        "",
-        "",
-        0, 0, 0,
-        "",
-        0, 0, 0,
-        "",
-        0,
-    };
-
-    return std::make_tuple(&rec, 0);
+void PriorityQueue::insert(AirportRecord rec) {
+    this->keys.push_back(rec);
+    this->heapify();
 }
 
-void PriorityQueue::increaseKey(AirportRecord *rec, int k) {
+tuple<AirportRecord, int> PriorityQueue::maximum() {
+    AirportRecord rec = this->keys.front();
+    return make_tuple(rec, rec.altitude);
+}
+
+tuple<AirportRecord, int> PriorityQueue::extractMax() {
+    auto max = this->maximum();
+
+    this->keys.erase(this->keys.begin());
+    this->heapify();
+
+    return max;
+}
+
+void PriorityQueue::heapify() {
 
 }
--- src/priority_queue.h
+++ src/priority_queue.h
@@ -1,25 +1,19 @@
-#ifndef PRIORITY_QUEUE_H
-#define PRIORITY_QUEUE_H
+#pragma once
 
 #include "airport_record.h"
 
 #include <tuple>
+#include <vector>
 
 class PriorityQueue {
     private:
-        class Node {
-            Node *parent;
-            Node *left;
-            Node *right;
-            AirportRecord* key;
-        };
+        vector<AirportRecord> keys;
 
-        Node *head;
+        void heapify();
     public:
-        void insert(AirportRecord *rec);
-        std::tuple<AirportRecord*, int> maximum();
-        std::tuple<AirportRecord*, int> extractMax();
-        void increaseKey(AirportRecord *rec, int k);
+        PriorityQueue(vector<AirportRecord> vec);
+        void insert(AirportRecord rec);
+        std::tuple<AirportRecord, int> maximum();
+        std::tuple<AirportRecord, int> extractMax();
+        void increaseKey(AirportRecord rec, int k);
 };
-
-#endif // PRIORITY_QUEUE_H
